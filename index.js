99294f115980e1896c6f11985d0bcdbb5c212931
kiieeaa <kiyaadinda5@gmail.com>
2025-10-22 11:05:29 +0700

    Initial Express server setup

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+let mysql = require("mysql2");
+const app = express();
+const PORT = 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+    res.send("Hello World!");
+});
+
+
